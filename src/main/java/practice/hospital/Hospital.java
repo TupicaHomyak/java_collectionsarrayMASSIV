694f5c59e4bd20f0fac8a068fb202771159e1705
Olga Malkova <Vodeviiil@mail.ru>
2023-04-28 20:13:36 +0300

    +10.6 hospital

--- src/main/java/practice/hospital/Hospital.java
+++ src/main/java/practice/hospital/Hospital.java
@@ -1,48 +1,52 @@
 package practice.hospital;
 
+import java.util.Arrays;
 import java.util.Random;
 
+import static net.sf.saxon.value.Whitespace.trim;
+
 public class Hospital {
+    static int max = 40;
+    static int min = 32;
+    static float minN = 36.2F;
+    static float maxN = 36.9F;
+    static String patientsTemperaturesAll = "";
+
+
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
-        float [] temperatures = new float[patientsCount];
-        Random random = new Random();
+        float [] patientTemperatures = new float[patientsCount];
         for (int i = 0; i < patientsCount; i++) {
-            float temperature = 32 + random.nextFloat() * 8; // случайная температура от 32 до 40 градусов
-            temperatures[i] = temperature;
+            float temperatureRandom = (float) ((Math.random() * (max -min))+ min); // случайная температура от 32 до 40 градусов
+            patientTemperatures[i] = (float) Math.ceil(temperatureRandom * 10) / 10;
         }
-        return temperatures;
+        return patientTemperatures;
     }
 
     public static String getReport(float[] temperatureData) {
-        float sum = 0;
+
+        float average = 0;
+        if (temperatureData.length > 0) {
+            float sum = 0;
+            for (int i = 0; i < temperatureData.length; i++) {
+                sum += temperatureData[i];
+                patientsTemperaturesAll += temperatureData[i] + " ";
+            }
+            average = (float) (Math.floor((sum / (temperatureData.length)) * 100) / 100);
+        }
+
         int healthyCount = 0;
-        StringBuilder reportBuilder = new StringBuilder();
-        for (float temperature : temperatureData) {
-            reportBuilder.append(String.format("%.1f", temperature)).append(" ");
-            sum += temperature;
-            if (temperature >= 36.2 && temperature <= 36.9) {
+        for (int i = 0; i < temperatureData.length; i++) {
+            if (temperatureData[i] > minN && temperatureData[i] <= maxN) {
                 healthyCount++;
             }
         }
-        float average = sum / temperatureData.length;
-        String averageFormatted = String.format("%.2f", average);
+
         String report =
-            "Температуры пациентов: " + reportBuilder +
-            "\nСредняя температура: " + averageFormatted +
+            "Температуры пациентов: " + patientsTemperaturesAll.trim() +
+            "\nСредняя температура: " + average +
             "\nКоличество здоровых: " + healthyCount;
 
         return report;
     }
 }
-
-//Напишите код метода generatePatientsTemperatures(),
-// который должен сгенерировать массив float,
-// содержащий значения температур пациентов от 32 до 40 градусов.
-//
-//
-// В методе getReport()вам требуется составить строку
-// с отчётом по значениям температур в массиве с пациентами:
-//Соберите в строку список температур пациентов.
-//Рассчитайте среднее арифметическое значение температуры.
-//Посчитайте количество здоровых пациентов с температурой от 36,2 до 36,9 градусов.
